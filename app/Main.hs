e058d972bb10a8ae72566190dea83284838e018c
Akshay Mankar <itsakshaymankar@gmail.com>
2018-11-17 09:15:16 +0100

    Add vars-file, ops-file and var options to CLI

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,51 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 module Main where
 
-import Bosh.Interpolation
-import Bosh.Interpolation.Types
-import Bosh.Operation
-import Bosh.Operation.Parsers
-import Bosh.Operation.Types
-import Data.HashMap.Strict as H
-import Bosh.Yaml
-import Data.Yaml
+import           Bosh.Yaml
+import qualified Data.Attoparsec.Text       as A
+import           Data.Text
+import           Options.Applicative        as O
+import           Options.Applicative.Simple (simpleOptions, simpleVersion)
+import           Options.Applicative.Text
+import           Options.Applicative.Types
+import           Paths_bosh_yaml_hs         as Meta
 
-import qualified Data.ByteString.Char8 as B
+attoReadM :: A.Parser a -> ReadM a
+attoReadM p = eitherReader (A.parseOnly p . pack)
+
+parseKeyValue :: A.Parser (Text, Text)
+parseKeyValue = do
+  key <- A.takeWhile (/= '=')
+  _ <- A.anyChar
+  value <- A.takeText
+  return (key, value)
+
+options :: Parser Options
+options = Options
+  <$> (many . textOption)
+    (long "ops-file"
+    <> short 'o'
+    <> metavar "PATH"
+    <> help "Load manifest operations from a YAML file")
+  <*> (fromM . manyM . option (attoReadM parseKeyValue))
+    (long "var"
+    <> short 'v'
+    <> metavar "VAR=VALUE"
+    <> help "Load manifest operations from a YAML file")
+  <*> (many . textOption)
+    (long "vars-file"
+    <> short 'l'
+    <> metavar "PATH"
+    <> help "Load variables from a YAML file")
+  <*> argument text (metavar "PATH" <> help "Path to a template that will be interpolated")
 
 main :: IO ()
 main = do
-  let vars = H.fromList [("this", "it"), ("also", "yes"), ("rk", "rm")]
-  eitherOps <- decodeFileEither "ops.yml" :: IO (Either ParseException [Operation])
-  case eitherOps of
-    (Right ops) -> do
-      eitherVal <- operate "foo.yml" ops vars
-      case eitherVal of
-        (Right v) -> B.putStrLn $ encode v
-        _ -> putStrLn "Error!!"
-    (Left s) -> do
-      _ <- print "Error"
-      print s
+  (opts, ()) <- simpleOptions
+                  $(simpleVersion Meta.version)
+                  "Bosh YAML"
+                  "Runs `bosh int` better than `bosh int`"
+                  options
+                  O.empty
+  run opts
--- src/Bosh/Yaml.hs
+++ src/Bosh/Yaml.hs
@@ -2,9 +2,11 @@ module Bosh.Yaml where
 
 import Bosh.Interpolation
 import Bosh.Operation
+import Bosh.Operation.Parsers
 import Bosh.Operation.Types
-import Control.Monad ((<=<))
-import Data.HashMap.Strict
+import Control.Monad          ((<=<))
+import Data.ByteString        as B (putStr)
+import Data.HashMap.Strict    as H
 import Data.Text
 import Data.Yaml
 
@@ -12,15 +14,52 @@ data BoshErr = YamlErr ParseException
              | OpErr OperationErr
   deriving (Show)
 
+data Options = Options { opsFiles  :: [Text]
+                       , vars      :: [(Text, Text)]
+                       , varsFiles :: [Text]
+                       , path      :: Text
+                       }
+  deriving Show
+
 wrapParseException :: Either ParseException Value -> Either BoshErr Value
-wrapParseException (Left l) = Left $ YamlErr l
+wrapParseException (Left l)  = Left $ YamlErr l
 wrapParseException (Right r) = Right r
 
 wrapOpErr :: Either OperationErr Value -> Either BoshErr Value
-wrapOpErr (Left l) = Left $ OpErr l
+wrapOpErr (Left l)  = Left $ OpErr l
 wrapOpErr (Right r) = Right r
 
 operate :: FilePath -> [Operation] -> HashMap Text Text -> IO (Either BoshErr Value)
 operate f ops vars = do
   doc <- wrapParseException <$> decodeFileEither f
   return $ (wrapOpErr . (flip interpolate vars <=< applyOps ops)) =<< doc
+
+readOpsFiles :: [FilePath] -> IO (Either ParseException [Operation])
+readOpsFiles files = do
+  opss <- mapM decodeFileEither files
+  return (Prelude.concat <$> sequence opss)
+
+zipM :: Monad m => m a -> m b -> m (a,b)
+zipM ma mb = do
+  a <- ma
+  b <- mb
+  return (a,b)
+
+readAllVars :: Options -> IO (Either ParseException (HashMap Text Text))
+readAllVars opts = do
+  eitherfileVars <- mapM (decodeFileEither . unpack) $ varsFiles opts
+  return $ do
+    fileVars <- Prelude.concat <$> sequence eitherfileVars
+    return $ H.fromList (vars opts ++ fileVars)
+
+run :: Options -> IO ()
+run opts = do
+  opsOrErr <- readOpsFiles $ unpack <$> opsFiles opts
+  varsOrErr <- readAllVars opts
+  case zipM opsOrErr varsOrErr of
+    Left e    -> putStrLn $ "Error!!\n" ++ show e
+    Right (ops, vars) -> do
+      res <- operate (unpack $ path opts) ops vars
+      case res of
+        Left e    -> putStrLn $ "Error!!\n" ++ show e
+        Right val -> B.putStr $ encode val
